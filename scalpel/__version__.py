c2619563ddcbca2e78ce30453dd893b46c55edd1
lewoudar <lewoudar@gmail.com>
2020-03-05 06:31:50 +0100

    feat(__version__): added a function get_version
    this function will return version information in a similar fashion like sys.version_info but with only major, minor and patch information

--- scalpel/__version__.py
+++ scalpel/__version__.py
@@ -1,1 +1,5 @@
 __version__ = '0.1.0'
+
+
+def version_info():
+    return tuple([int(i) for i in __version__.split('.')])
--- tests/test_version.py
+++ tests/test_version.py
@@ -1,7 +1,15 @@
 import re
 
-from scalpel.__version__ import __version__
+from scalpel.__version__ import __version__, version_info
 
 
 def test_version_is_correctly_formatted():
     assert re.match(r'(\d+\.){2}\d+', __version__)
+
+
+def test_version_info_returns_correct_version():
+    value = version_info()
+    assert isinstance(value, tuple)
+    assert 3 == len(value)
+    for item in value:
+        assert isinstance(item, int)
