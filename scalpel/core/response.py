a8b1e7ff870995649389ad56e26114297f27982b
lewoudar <lewoudar@gmail.com>
2020-05-02 01:07:15 +0200

    feat(response): implement method _join_url in BaseStaticResponse class

--- scalpel/core/response.py
+++ scalpel/core/response.py
@@ -1,44 +1,64 @@
 import logging
-from typing import Dict
+from typing import Dict, Optional
 
 import attr
 import httpx
 import parsel
+from rfc3986 import uri_reference
 
 logger = logging.getLogger('scalpel')
 
 
 @attr.s
 class Response:
-    _httpx_response: httpx.Response = attr.ib()
+    _url: str = attr.ib(default='', validator=attr.validators.optional(attr.validators.instance_of(str)), kw_only=True)
+    _text: str = attr.ib(default='', validator=attr.validators.instance_of(str), kw_only=True)
+    _httpx_response: Optional[httpx.Response] = attr.ib(
+        default=None, validator=attr.validators.optional(attr.validators.instance_of(httpx.Response)), kw_only=True
+    )
 
     @property
     def url(self) -> str:
-        _url = f'{self._httpx_response.url}'
+        if self._url:
+            _url = self._url
+        else:
+            _url = f'{self._httpx_response.url}'
         logger.debug('returning response url: %s', _url)
         return _url
 
     @property
     def headers(self) -> Dict[str, str]:
-        _headers = dict(self._httpx_response.headers)
+        if self._url:
+            _headers = {}
+        else:
+            _headers = dict(self._httpx_response.headers)
         logger.debug('returning response headers:\n %s', _headers)
         return _headers
 
     @property
     def text(self) -> str:
-        _text = self._httpx_response.text
+        if self._text:
+            _text = self._text
+        else:
+            _text = self._httpx_response.text
         logger.debug('returning response text content:\n %s', _text)
         return _text
 
     @property
     def content(self) -> bytes:
-        _content = self._httpx_response.content
+        if self._text:
+            _content = self._text.encode()
+        else:
+            _content = self._httpx_response.content
         logger.debug('returning response byte content:\n %s', _content)
         return _content
 
     @property
     def cookies(self) -> Dict[str, str]:
-        _cookies = dict(self._httpx_response.cookies)
+        if self._url:
+            _cookies = {}
+        else:
+            _cookies = dict(self._httpx_response.cookies)
         logger.debug('returning response cookies: %s', _cookies)
         return _cookies
 
@@ -49,7 +69,8 @@ class BaseStaticResponse(Response):
 
     @_selector.default
     def get_selector(self) -> parsel.Selector:
-        return parsel.Selector(self._httpx_response.text)
+        logger.debug('creating parsel selector with text:\n %s', self.text)
+        return parsel.Selector(self.text)
 
     def css(self, query: str) -> parsel.SelectorList:
         logger.debug('selecting content using css selector: %s', query)
@@ -58,3 +79,18 @@ class BaseStaticResponse(Response):
     def xpath(self, query: str) -> parsel.SelectorList:
         logger.debug('selecting content using xpath selector: %s', query)
         return self._selector.xpath(query)
+
+    def _join_url(self, url: str) -> str:
+        if self._url:
+            absolute_uri = uri_reference(self._url)
+            given_uri = uri_reference(url)
+            if given_uri.is_absolute():
+                _url = url
+            else:
+                uri = given_uri.resolve_with(absolute_uri)
+                uri = uri.copy_with(fragment=None)
+                _url = uri.unsplit()
+        else:
+            _url = f'{self._httpx_response.url.join(url)}'
+        logger.debug('returning new joined url: %s', _url)
+        return _url
--- tests/core/test_response.py
+++ tests/core/test_response.py
@@ -1,5 +1,8 @@
+from typing import Union
+
 import attr
 import httpx
+import parsel
 import pytest
 
 from scalpel.core.response import Response, BaseStaticResponse
@@ -18,42 +21,85 @@ def httpx_response(dummy_data):
 
 
 class TestResponse:
-    """Tests class Response"""
+    """Tests Response class"""
 
-    def test_should_validate_properties(self):
+    def test_should_validate_properties_when_passing_url_and_text(self, dummy_data):
+        response = Response(url='http://foo.com', text=dummy_data.decode())
+
+        assert 'http://foo.com' == response.url
+        assert_dicts({}, response.headers)
+        assert_dicts({}, response.cookies)
+        assert dummy_data.decode() == response.text
+        assert dummy_data == response.content
+
+    def test_should_validate_properties_when_passing_httpx_response(self):
         url = 'http://foobar.com'
         request = httpx.Request('GET', url)
         headers = {'foo': 'bar', 'set-cookie': 'name=John'}
         content = b'hello world'
         httpx_response = httpx.Response(200, request=request, headers=headers, content=content)
-        response = Response(httpx_response)
+        response = Response(httpx_response=httpx_response)
 
         assert url == response.url
         assert_dicts(headers, response.headers)
-        assert {'name': 'John'} == response.cookies
+        assert_dicts({'name': 'John'}, response.cookies)
         assert content == response.content
         assert content.decode() == response.text
 
     def test_should_validate_attributes(self):
         fields = {attribute.name: attribute.type for attribute in attr.fields(Response)}
-        assert {'_httpx_response': httpx.Response} == fields
+        attributes = {
+            '_httpx_response': Union[httpx._models.Response, type(None)],
+            '_url': str,
+            '_text': str
+        }
+        assert_dicts(fields, attributes)
 
 
-class TestAbstractStaticResponse:
+class TestBaseStaticResponse:
     """Tests class BaseStaticResponse"""
 
     def test_should_validate_attributes(self):
-        fields = {attribute.name: attribute.type for attribute in attr.fields(Response)}
-        assert {'_httpx_response': httpx.Response} == fields
+        fields = {attribute.name: attribute.type for attribute in attr.fields(BaseStaticResponse)}
+        attributes = {
+            '_httpx_response': Union[httpx._models.Response, type(None)],
+            '_url': str,
+            '_text': str,
+            '_selector': parsel.Selector
+        }
+        assert_dicts(fields, attributes)
 
     def test_should_validate_that_selector_attribute_is_correctly_instantiated(self, dummy_data, httpx_response):
-        response = BaseStaticResponse(httpx_response)
+        response = BaseStaticResponse(httpx_response=httpx_response)
         assert response._selector.get() == dummy_data.decode()
 
     def test_should_return_correct_data_when_calling_css_method(self, httpx_response):
-        response = BaseStaticResponse(httpx_response)
+        response = BaseStaticResponse(httpx_response=httpx_response)
         assert '<p>Hello World!</p>' == response.css('p').get()
 
     def test_should_return_correct_data_when_calling_xpath_method(self, httpx_response):
-        response = BaseStaticResponse(httpx_response)
+        response = BaseStaticResponse(httpx_response=httpx_response)
         assert '<p>Hello World!</p>' == response.xpath('//p').get()
+
+    # tests method _join_url
+
+    @pytest.mark.parametrize(('given_url', 'joined_url'), [
+        ('hello', 'http://foobar.com/hello'),
+        ('/hello', 'http://foobar.com/hello'),
+        ('#hello', 'http://foobar.com#hello'),
+        ('http://example.com', 'http://example.com')
+    ])
+    def test_should_return_joined_url_given_httpx_response(self, httpx_response, given_url, joined_url):
+        response = BaseStaticResponse(httpx_response=httpx_response)
+        assert joined_url == response._join_url(given_url)
+
+    @pytest.mark.parametrize(('given_url', 'joined_url'), [
+        ('page.html', 'file:/C/foo/page.html'),
+        ('/page.html', 'file:/page.html'),
+        ('#page', 'file:/C/foo/bar.html'),
+        ('http://foo.com', 'http://foo.com'),
+        ('file:///C:/path/to/file', 'file:///C:/path/to/file')
+    ])
+    def test_should_return_joined_url_given_url_and_text(self, dummy_data, given_url, joined_url):
+        response = BaseStaticResponse(url='file:/C/foo/bar.html', text=dummy_data.decode())
+        assert joined_url == response._join_url(given_url)
