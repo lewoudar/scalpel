401528036ffe6cc85364fe9ae14a7ca2600b8f93
lewoudar <lewoudar@gmail.com>
2020-05-30 22:58:05 +0200

    feat(green): added functions open_file and wrap_file at top level package imports

--- scalpel/green/__init__.py
+++ scalpel/green/__init__.py
@@ -2,8 +2,11 @@ from gevent import monkey
 
 monkey.patch_all()  # noqa: E402
 from .files import read_mp, write_mp
+from .utils.io import wrap_file, open_file
 
 __all__ = [
     # files
-    'read_mp', 'write_mp'
+    'read_mp', 'write_mp',
+    # io
+    'wrap_file', 'open_file'
 ]
