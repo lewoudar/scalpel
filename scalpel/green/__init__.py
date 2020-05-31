8ab1c83f7bd95769130e4e0508d809d522d69c4e
lewoudar <lewoudar@gmail.com>
2020-05-31 22:21:15 +0200

    tests(flake8): updated noqa comments in green/__init__.py
    This solved a changed behaviour introduced in flake8 3.8.*

--- scalpel/green/__init__.py
+++ scalpel/green/__init__.py
@@ -1,8 +1,8 @@
 from gevent import monkey
 
-monkey.patch_all()  # noqa: E402
-from .files import read_mp, write_mp
-from .utils.io import wrap_file, open_file
+monkey.patch_all()
+from .files import read_mp, write_mp  # noqa: E402
+from .utils.io import wrap_file, open_file  # noqa: E402
 
 __all__ = [
     # files
