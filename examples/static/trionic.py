0d60de4a0d14e5272de0a5c0beb0586fc818733e
lewoudar <rollandkev@yahoo.fr>
2020-09-05 14:42:06 +0200

    docs(examples): add an example of a trio static spider

--- examples/static/trionic.py
+++ examples/static/trionic.py
@@ -1,23 +1,25 @@
-"""Simple example to show how to use the green StaticSpider class"""
+"""Simple example to show how to use the trio StaticSpider class"""
 from datetime import datetime
 from pathlib import Path
 
+import trio
+
 from scalpel import Configuration, datetime_decoder
-from scalpel.green import StaticSpider, StaticResponse, read_mp
+from scalpel.trionic import StaticResponse, StaticSpider, read_mp
 
 
-def parse(spider: StaticSpider, response: StaticResponse) -> None:
+async def parse(spider: StaticSpider, response: StaticResponse) -> None:
     for quote in response.xpath('//div[@class="quote"]'):
         data = {
             'message': quote.xpath('./span[@class="text"]/text()').get(),
             'author': quote.xpath('./span/small/text()').get(),
             'tags': quote.xpath('./div/a/text()').getall()
         }
-        spider.save_item(data)
+        await spider.save_item(data)
 
     next_link = response.xpath('//nav/ul/li[@class="next"]/a').xpath('@href').get()
     if next_link is not None:
-        response.follow(next_link)
+        await response.follow(next_link)
 
 
 def date_processor(item: dict) -> dict:
@@ -25,12 +27,16 @@ def date_processor(item: dict) -> dict:
     return item
 
 
-if __name__ == '__main__':
+async def main():
     backup = Path(__file__).parent / 'backup.mp'
     config = Configuration(backup_filename=f'{backup}', item_processors=[date_processor])
     spider = StaticSpider(urls=['http://quotes.toscrape.com'], parse=parse, config=config)
-    spider.run()
+    await spider.run()
     print(spider.statistics())
     # you can do whatever you want with the results
-    for item in read_mp(filename=backup, decoder=datetime_decoder):
+    async for item in read_mp(backup, decoder=datetime_decoder):
         print(item)
+
+
+if __name__ == '__main__':
+    trio.run(main)
--- scalpel/trionic/__init__.py
+++ scalpel/trionic/__init__.py
@@ -1,6 +1,15 @@
 from .files import read_mp, write_mp
+from .response import StaticResponse
+from .static_spider import StaticSpider
+from .utils.queue import Queue
 
 __all__ = [
     # files
-    'read_mp', 'write_mp'
+    'read_mp', 'write_mp',
+    # spider
+    'StaticSpider',
+    # response
+    'StaticResponse',
+    # queue
+    'Queue'
 ]
