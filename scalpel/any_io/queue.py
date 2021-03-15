4a3895bbe0914bbf61bdd3534319a3bcaff1cc04
lewoudar <rollandkev@yahoo.fr>
2021-03-15 23:50:56 +0100

    feat(any_io): implement class Queue

---
+++ scalpel/any_io/queue.py
@@ -0,0 +1,155 @@
+"""Implementation of an anyio queue."""
+import logging
+from typing import Iterable, Any, Union
+
+import anyio
+import attr
+from anyio.abc import Event
+from anyio.streams.memory import MemoryObjectSendStream, MemoryObjectReceiveStream
+
+logger = logging.getLogger('scalpel')
+
+
+@attr.s
+class Queue:
+    """
+    An implementation of an anyio queue with the capacity to tell that a task is done.
+
+    **Parameters:**
+
+    * **size:** The size of the queue, defaults to 1 (this is the minimum value).
+    * **items:** A list of values used to initialize the queue.
+
+    **N.B:** If you set `items`, be sure that the size of the queue is greater than `items` length, otherwise you will
+    have a `anyio.WouldBlock` exception.
+
+    ```python
+    import math
+    import random
+
+    import anyio
+    from scalpel.any_io import Queue
+
+    async def worker(name: str, queue: Queue) -> None:
+        while True:
+            # Get a "work item" out of the queue.
+            time_to_sleep = await queue.get()
+
+            await trio.sleep(time_to_sleep)
+            # Notify the queue that the "work item" has been processed.
+            queue.task_done()
+            print(f'{name} has slept for {time_to_sleep:.2f} seconds')
+
+
+    async def main():
+        async with Queue(math.inf) as queue:
+            # Generate random timings and put them into the queue.
+            total_sleep_time = 0
+            for _ in range(20):
+                sleep_for = random.uniform(0.05, 1.0)
+                total_sleep_time += sleep_for
+                queue.put_nowait(sleep_for)
+
+            # Create three worker tasks to process the queue concurrently.
+            async with anyio.create_task_group() as tg:
+                for i in range(3):
+                    tg.spawn(worker, f'worker-{i}', queue)
+
+                # Wait until the queue is fully processed.
+                before = anyio.current_time()
+                await queue.join()
+                total_slept_for = trio.current_time() - before
+                print('====')
+                print(f'3 workers slept in parallel for {total_slept_for:.2f} seconds')
+                print(f'total expected sleep time: {total_sleep_time:.2f} seconds')
+
+                # We can end the nursery which will in turn end the tasks.
+                tg.cancel_scope.cancel()
+
+
+    anyio.run(main)
+    ```
+    """
+    _size: Union[int, float] = attr.ib(default=1, validator=attr.validators.instance_of((int, float)))
+    _items: Iterable[Any] = attr.ib(factory=list, validator=attr.validators.instance_of((list, set, tuple)))
+    _send_channel: MemoryObjectSendStream = attr.ib(init=False)
+    _receive_channel: MemoryObjectReceiveStream = attr.ib(init=False)
+    _tasks_in_progress: int = attr.ib(default=0, init=False)
+    _finished: Event = attr.ib(factory=anyio.create_event, init=False)
+
+    def __attrs_post_init__(self):
+        logger.debug('initializing queue send and receive channels with a size of %s', self._size)
+        self._send_channel, self._receive_channel = anyio.create_memory_object_stream(self._size)
+        if self._items:
+            logger.debug('trying to add items coming from list %s', self._items)
+            for item in self._items:
+                self._tasks_in_progress += 1
+                self._send_channel.send_nowait(item)
+
+    @_size.validator
+    def _validate_size(self, _, value: int) -> None:
+        if value < 1:
+            message = f'size must not be less than 1 but you provide: {value}'
+            logger.error(message)
+            raise ValueError(message)
+
+    @property
+    def length(self) -> int:
+        """Tne number of items currently present in the queue."""
+        value = self._send_channel.statistics().current_buffer_used
+        logger.debug('returning queue length: %s', value)
+        return value
+
+    @property
+    def maxsize(self) -> Union[int, float]:
+        """The number of items allowed in the queue."""
+        logger.debug('returning queue max size: %s', self._size)
+        return self._size
+
+    async def put(self, item: Any) -> None:
+        logger.debug('adding item %s to queue', item)
+        await self._send_channel.send(item)
+        self._tasks_in_progress += 1
+        logger.debug('number of tasks in progress is now: %s', self._tasks_in_progress)
+        if self._finished.is_set():
+            self._finished = anyio.create_event()
+
+    def put_nowait(self, item: Any) -> None:
+        logger.debug('trying to add %s to queue', item)
+        self._send_channel.send_nowait(item)
+        self._tasks_in_progress += 1
+        logger.debug('number of tasks in progress is now: %s', self._tasks_in_progress)
+        if self._finished.is_set():
+            self._finished = anyio.create_event()
+
+    async def get(self) -> Any:
+        item = await self._receive_channel.receive()
+        logger.debug('returning item %s from queue', item)
+        return item
+
+    def get_nowait(self) -> Any:
+        logger.debug('trying to get item from queue')
+        return self._receive_channel.receive_nowait()
+
+    def task_done(self) -> None:
+        if self._tasks_in_progress <= 0:
+            raise ValueError('task_done method was calling too many times without adding items in queue')
+        self._tasks_in_progress -= 1
+        logger.debug('decrementing tasks in progress, now the new value is: %s', self._tasks_in_progress)
+        if self._tasks_in_progress == 0:
+            self._finished.set()
+
+    async def close(self) -> None:
+        logger.debug('closing send and receive channels')
+        await self._send_channel.aclose()
+        await self._receive_channel.aclose()
+
+    async def join(self) -> None:
+        logger.debug('waiting self._tasks_in_progress to be equal to 0')
+        await self._finished.wait()
+
+    async def __aenter__(self):
+        return self
+
+    async def __aexit__(self, exc_type, exc_val, exc_tb):
+        await self.close()
---
+++ tests/any_io/test_queue.py
@@ -0,0 +1,176 @@
+import math
+
+import anyio
+from anyio.abc import Event
+from anyio.streams.memory import MemoryObjectSendStream, MemoryObjectReceiveStream
+import pytest
+
+from scalpel.any_io.queue import Queue
+
+pytestmark = pytest.mark.anyio
+
+
+# since in the __init__ method we call anyio.create_event function which tries to guess
+# the async library, we must use async test functions even if we don't have async call in them
+class TestQueueInitialization:
+
+    async def test_should_work_with_default_initialization(self):
+        queue = Queue()
+        assert 1 == queue.maxsize
+        assert 0 == queue.length
+        assert [] == queue._items
+        assert 0 == queue._tasks_in_progress
+        assert isinstance(queue._send_channel, MemoryObjectSendStream)
+        assert isinstance(queue._receive_channel, MemoryObjectReceiveStream)
+        assert isinstance(queue._finished, Event)
+
+    async def test_should_work_when_initializing_buffer_size(self):
+        queue = Queue(size=3)
+        assert 3 == queue.maxsize
+        assert 0 == queue.length
+
+    async def test_should_work_when_initializing_items(self):
+        items = [2, 'foo']
+        queue = Queue(3, items=items)
+        assert 3 == queue.maxsize
+        assert items == queue._items
+        assert len(items) == queue.length == queue._tasks_in_progress
+
+    async def test_length_property_works_with_infinity_size(self):
+        queue = Queue(size=math.inf)
+        assert 0 == queue.length
+        assert queue.maxsize is math.inf
+
+    async def test_should_work_when_initializing_with_size_and_items(self):
+        items = [2, 'foo']
+        queue = Queue(size=3, items=items)
+        assert 3 == queue.maxsize
+        assert 2 == queue.length
+        assert items == queue._items
+
+    async def test_should_raise_error_when_size_is_negative(self):
+        with pytest.raises(ValueError) as exc_info:
+            Queue(size=-1)
+
+        assert 'size must not be less than 1 but you provide: -1' == str(exc_info.value)
+
+    async def test_should_raise_error_when_size_is_less_than_items_length(self):
+        with pytest.raises(anyio.WouldBlock):
+            Queue(size=1, items=[2, 'foo'])
+
+
+class TestPutMethods:
+    """Tests methods put and put_nowait"""
+
+    async def test_should_work_when_adding_item_with_put_method(self):
+        queue = Queue(size=3)
+        await queue.put(2)
+        await queue.put('foo')
+
+        assert 2 == queue.length
+        assert 2 == queue._tasks_in_progress
+
+    async def test_should_work_when_adding_item_with_put_nowait_method(self):
+        queue = Queue(size=3)
+        queue.put_nowait(2)
+
+        assert 1 == queue.length
+        assert 1 == queue._tasks_in_progress
+
+    async def test_should_raise_would_block_error_when_buffer_is_full_and_put_nowait_is_used(self):
+        queue = Queue()
+        queue.put_nowait(1)
+
+        with pytest.raises(anyio.WouldBlock):
+            queue.put_nowait(2)
+
+    # noinspection PyAsyncCall
+    async def test_should_reset_event_when_event_is_set_and_put_method_is_called(self):
+        queue = Queue()
+        queue._finished.set()
+        await queue.put(2)
+
+        assert not queue._finished.is_set()
+
+    # noinspection PyAsyncCall
+    async def test_should_reset_event_when_event_is_set_and_put_nowait_is_called(self):
+        queue = Queue()
+        queue._finished.set()
+        queue.put_nowait(2)
+
+        assert not queue._finished.is_set()
+
+
+class TestGetMethods:
+    """Tests methods get and get_nowait"""
+
+    async def test_should_work_when_using_get_method(self):
+        queue = Queue(3, items=[2, 'foo'])
+
+        assert 2 == await queue.get()
+        assert 'foo' == await queue.get()
+
+    async def test_should_work_when_using_get_nowait_method(self):
+        queue = Queue(3, items=[2, 'foo'])
+
+        assert 2 == queue.get_nowait()
+        assert 'foo' == queue.get_nowait()
+
+    async def test_should_raise_would_block_error_when_using_get_nowait_on_empty_buffer(self):
+        queue = Queue()
+
+        with pytest.raises(anyio.WouldBlock):
+            queue.get_nowait()
+
+
+class TestCloseMethod:
+    """Tests method close"""
+
+    async def test_should_work_when_calling_close_method(self):
+        queue = Queue()
+        await queue.close()
+
+        with pytest.raises(anyio.ClosedResourceError):
+            await queue.put(2)
+
+        with pytest.raises(anyio.ClosedResourceError):
+            await queue.get()
+
+    async def test_should_work_when_using_context_manager(self):
+        async with Queue(size=1) as queue:
+            queue.put_nowait(2)
+
+        with pytest.raises(anyio.ClosedResourceError):
+            queue.put_nowait(3)
+
+        with pytest.raises(anyio.ClosedResourceError):
+            queue.get_nowait()
+
+
+class TestTaskDoneAndJoinMethods:
+    """Tests methods task_done and join"""
+
+    async def test_should_raise_error_when_task_done_is_called_without_adding_items_in_queue(self):
+        async with Queue(size=1) as queue:
+            with pytest.raises(ValueError) as exc_info:
+                queue.task_done()
+
+            assert 'task_done method was calling too many times without adding items in queue' == str(exc_info.value)
+
+    async def test_should_work_when_using_task_done_and_join_methods(self):
+        async with Queue(size=1) as queue:
+            await queue.put(2)
+            await queue.get()
+            assert 1 == queue._tasks_in_progress
+
+            with anyio.move_on_after(1) as cancel_scope:
+                await queue.join()
+
+            assert cancel_scope.cancel_called
+            queue.task_done()
+            assert 0 == queue._tasks_in_progress
+
+            with anyio.move_on_after(1) as cancel_scope:
+                await queue.join()
+
+            assert not cancel_scope.cancel_called
