51cf27e042f38856ed09a477d3a58a4202357052
lewoudar <rollandkev@yahoo.fr>
2021-03-21 21:47:11 +0100

    feat(any_io/static_spider): implemented static spider

---
+++ scalpel/any_io/static_spider.py
@@ -0,0 +1,205 @@
+import logging
+import math
+import platform
+from asyncio import iscoroutinefunction
+from pathlib import Path
+from typing import Callable, Optional, Any, Union
+
+import anyio
+import attr
+import httpx
+# noinspection PyProtectedMember
+from anyio._core._synchronization import Lock
+from anyio.abc import TaskGroup
+from rfc3986 import uri_reference
+
+from scalpel.core.spider import Spider
+from .files import write_mp
+from .response import StaticResponse
+from .robots import RobotsAnalyzer
+from .queue import Queue
+
+logger = logging.getLogger('scalpel')
+
+
+@attr.s(slots=True)
+class StaticSpider(Spider):
+    """
+    A spider suitable to parse files or static HTML files.
+
+    **Parameters:**
+
+    * **urls:** Urls to parse. Allowed schemes are `http`, `https` and `file`. It can be a `list`, a `tuple` or a `set`.
+    * **parse:** An async callable used to parse url content. It takes two arguments: the current spider and a
+    `StaticResponse` object.
+    * **reachable_urls:** `set` of urls that are already fetched or read.
+    * **unreachable_urls:** `set` of urls that were impossible to fetch or read.
+    * **robot_excluded_urls:** `set` of urls that were excluded to fetch because of *robots.txt* file rules.
+    * **followed_urls:** `set` of urls that were followed during the process of parsing url content. You will find these
+    urls scattered in the first three sets.
+    * **request_counter:** The number of urls already fetched or read.
+
+    Usage:
+
+    ```
+    from scalpel.any_io import StaticSpider, StaticResponse
+
+    async def parse(spider: StaticSpider, response: StaticResponse) -> None:
+        ...
+
+    spider = StaticSpider(urls=['http://example.com'], parse=parse)
+    await spider.run()
+    ```
+    """
+    # order is important here, http_client must come before robots_analyzer since the latter used the former
+    _start_time: float = attr.ib(init=False, repr=False, factory=anyio.current_time)
+    _http_client: httpx.AsyncClient = attr.ib(init=False, repr=False)
+    _robots_analyser: RobotsAnalyzer = attr.ib(init=False, repr=False)
+    _fetch: Callable = attr.ib(init=False, repr=False)
+    _lock: Lock = attr.ib(init=False, repr=False, factory=anyio.create_lock)
+    _queue: Queue = attr.ib(init=False, repr=False)
+
+    def __attrs_post_init__(self):
+
+        async def _get_fetch(url: str) -> httpx.Response:
+            return await self._http_client.get(url)
+
+        self._fetch = _get_fetch
+        for middleware in self.config.response_middlewares:
+            self._fetch = middleware(self._fetch)
+
+    @_http_client.default
+    def _get_http_client(self) -> httpx.AsyncClient:
+        headers = {'User-Agent': self.config.user_agent}
+        logger.debug('getting a default httpx client with user agent: %s', self.config.user_agent)
+        return httpx.AsyncClient(headers=headers, timeout=self.config.fetch_timeout)
+
+    @_robots_analyser.default
+    def _get_robots_analyzer(self) -> RobotsAnalyzer:
+        logger.debug('getting a default robots analyzer')
+        return RobotsAnalyzer(
+            http_client=self._http_client,
+            robots_cache=Path(self.config.robots_cache_folder),
+            user_agent=self.config.user_agent
+        )
+
+    @_queue.default
+    def _get_queue(self) -> Queue:
+        logger.debug('getting a default queue')
+        return Queue(size=math.inf, items=self.urls)
+
+    def _get_static_response(
+            self, url: str = '', text: str = '', httpx_response: httpx.Response = None
+    ) -> StaticResponse:
+        logger.debug(
+            'returning StaticResponse object with url: %s, text: %s and httpx_response: %s', url, text, httpx_response
+        )
+        return StaticResponse(
+            self.reachable_urls, self.followed_urls, self._queue, url=url, text=text,
+            httpx_response=httpx_response
+        )
+
+    def _is_url_already_processed(self, url: str) -> False:
+        processed = False
+        if url in [*self.reachable_urls, *self.unreachable_urls, *self.robots_excluded_urls]:
+            logger.debug('url %s has already been processed', url)
+            self._queue.task_done()
+            processed = True
+        return processed
+
+    # noinspection PyBroadException
+    async def _handle_url(self, url: str) -> None:
+        if self._is_url_already_processed(url):
+            return
+
+        static_url = text = ''
+        response: Optional[httpx.Response] = None
+        ur = uri_reference(url)
+        if ur.scheme == 'file':
+            static_url = url
+            logger.debug('url %s is a file url so we attempt to read its content')
+            file_path = ur.path[1:] if platform.system() == 'Windows' else ur.path
+            try:
+                before = anyio.current_time()
+                async with await anyio.open_file(file_path) as f:
+                    text = await f.read()
+                fetch_time = anyio.current_time() - before
+            except OSError:
+                logger.exception('unable to open file %s', url)
+                self.unreachable_urls.add(url)
+                return
+        else:
+            response: httpx.Response = await self._fetch(url)
+            if response.is_error:
+                logger.info('fetching url %s returns an error with status code %s', url, response.status_code)
+                self.unreachable_urls.add(url)
+                return
+            fetch_time = response.elapsed.total_seconds()
+
+        # we update some variables for statistics
+        self.request_counter += 1
+        self.reachable_urls.add(url)
+        self._total_fetch_time += fetch_time
+
+        try:
+            await self.parse(self, self._get_static_response(static_url, text, response))
+        except Exception:
+            logger.exception('something unexpected happened while parsing the content at url %s', url)
+            if not self._ignore_errors:
+                self._queue.task_done()
+                raise
+        self._queue.task_done()
+        logger.info('content at url %s has been processed', url)
+
+    async def save_item(self, item: Any) -> None:
+        item_rejected = False
+        original_item = item
+        for processor in self.config.item_processors:
+            if iscoroutinefunction(processor):
+                item = await processor(item)
+            else:
+                item = processor(item)
+            if item is None:
+                item_rejected = True
+                break
+        if item_rejected:
+            logger.debug('item %s was rejected', original_item)
+            return
+
+        logger.debug('writing item %s to file %s', item, self.config.backup_filename)
+        async with self._lock:
+            await write_mp(self.config.backup_filename, item, mode='a', encoder=self.config.msgpack_encoder)
+
+    async def _get_request_delay(self, url: str) -> Union[int, float]:
+        if self.config.follow_robots_txt:
+            return await self._robots_analyser.get_request_delay(url, self.config.request_delay)
+        await anyio.sleep(0)  # checkpoint to ensure an async function in every case
+        return self.config.request_delay
+
+    async def worker(self, task_group: TaskGroup) -> None:
+        while True:
+            url = await self._queue.get()
+            request_delay = await self._get_request_delay(url)
+            if request_delay == -1:  # url is not accessible
+                self.robots_excluded_urls.add(url)
+                self._queue.task_done()
+                continue
+            # noinspection PyAsyncCall
+            task_group.spawn(self._handle_url, url)
+            await anyio.sleep(request_delay)
+
+    async def _cleanup(self) -> None:
+        await self._http_client.aclose()
+        await self._queue.close()
+
+    # noinspection PyAsyncCall
+    async def run(self) -> None:
+        """Runs the spider."""
+        async with anyio.create_task_group() as tg:
+            tg.spawn(self.worker, tg)
+            await self._queue.join()
+            # at this point, all the urls were handled, so the only remaining task is the worker
+            tg.cancel_scope.cancel()
+
+        await self._cleanup()
+        self._duration = anyio.current_time() - self._start_time
---
+++ tests/any_io/test_static_spider.py
@@ -0,0 +1,398 @@
+from datetime import datetime
+from pathlib import Path
+
+import anyio
+import httpx
+import pytest
+import respx
+# noinspection PyProtectedMember
+from anyio._core._synchronization import Lock
+
+from scalpel.core.config import Configuration
+from scalpel.core.message_pack import datetime_decoder
+from scalpel.core.spider import SpiderStatistics
+from scalpel.any_io.files import read_mp
+from scalpel.any_io.response import StaticResponse
+from scalpel.any_io.robots import RobotsAnalyzer
+from scalpel.any_io.static_spider import StaticSpider
+from scalpel.any_io.queue import Queue
+
+
+@pytest.fixture()
+async def anyio_spider():
+    return StaticSpider(
+        urls=['http://foo.com', 'http://bar.com', 'file:///home/kevin/page.html'], parse=lambda x, y: None
+    )
+
+
+class TestStaticSpider:
+    """Tests class StaticSpider"""
+
+    async def test_specific_static_attributes_are_correctly_instantiated(self):
+        config = Configuration(user_agent='mozilla/5.0')
+        spider = StaticSpider(urls=['http://foo.com'], parse=lambda x, y: None, config=config)
+
+        assert config == spider._config
+        assert isinstance(spider._lock, Lock)
+        assert isinstance(spider._queue, Queue)
+        assert isinstance(spider._start_time, float)
+        assert isinstance(spider._http_client, httpx.AsyncClient)
+        assert isinstance(spider._robots_analyser, RobotsAnalyzer)
+
+    # _fetch tests
+
+    @respx.mock
+    async def test_fetch_method_returns_httpx_response(self, anyio_spider):
+        url = 'http://foo.com'
+        respx.get('http://foo.com') % {'text': 'content'}
+        response = await anyio_spider._fetch(url)
+
+        assert 'content' == response.text
+        assert 200 == response.status_code
+        assert url == f'{response.url}'
+
+    @respx.mock
+    async def test_middlewares_are_applied_when_fetching_resources(self, capsys):
+        def log_middleware(fetch):
+            async def wrapper(*args, **kwargs):
+                print('before fetching')
+                return await fetch(*args, **kwargs)
+
+            print('after fetching')
+            return wrapper
+
+        url = 'http://foo.com'
+        respx.get(url)
+        config = Configuration(response_middlewares=[log_middleware])
+        spider = StaticSpider(urls=[url], parse=lambda x, y: None, config=config)
+        response = await spider._fetch(url)
+
+        assert 200 == response.status_code
+        out, _ = capsys.readouterr()
+        assert 'before fetching' in out
+        assert 'after fetching' in out
+
+    # _get_static_response test
+
+    @pytest.mark.parametrize(('url', 'text', 'httpx_response'), [
+        ('file:///home/kevin/page.html', 'hello world', None),
+        ('', '', httpx.Response(200, request=httpx.Request('GET', 'http://foo.com')))
+    ])
+    async def test_should_return_static_response_when_giving_correct_input(
+            self, url, text, httpx_response, anyio_spider
+    ):
+        static_response = anyio_spider._get_static_response(url, text, httpx_response)
+
+        assert isinstance(static_response, StaticResponse)
+        assert static_response._reachable_urls is anyio_spider.reachable_urls
+        assert static_response._followed_urls is anyio_spider.followed_urls
+        assert static_response._queue is anyio_spider._queue
+        assert url == static_response._url
+        assert text == static_response._text
+        assert static_response._httpx_response is httpx_response
+
+    # _handle_url tests
+
+    @pytest.mark.parametrize(('reachable_urls', 'unreachable_urls', 'robots_excluded_urls'), [
+        (set(), set(), {'http://foo.com'}),
+        (set(), {'http://foo.com'}, set()),
+        ({'http://foo.com'}, set(), set())
+    ])
+    async def test_should_do_nothing_if_url_is_already_present_in_one_url_set(
+            self, mocker, anyio_spider, reachable_urls, unreachable_urls, robots_excluded_urls
+    ):
+        url = 'http://foo.com'
+        logger_mock = mocker.patch('logging.Logger.debug')
+        anyio_spider.reachable_urls = reachable_urls
+        anyio_spider.unreachable_urls = unreachable_urls
+        anyio_spider.robots_excluded_urls = robots_excluded_urls
+        await anyio_spider._handle_url(url)
+
+        logger_mock.assert_any_call('url %s has already been processed', url)
+
+    async def test_should_read_file_content_when_giving_a_file_url(self, tmp_path):
+        parse_args = []
+        hello_file = tmp_path / 'hello.txt'
+        hello_file.write_text('hello world')
+        file_url = hello_file.resolve().as_uri()
+
+        async def parse(spider, response):
+            parse_args.extend([spider, response])
+
+        static_spider = StaticSpider(urls=[file_url], parse=parse)
+        await static_spider._handle_url(file_url)
+
+        assert parse_args[0] is static_spider
+        static_response = parse_args[1]
+        assert isinstance(static_response, StaticResponse)
+        assert file_url == static_response._url
+        assert 'hello world' == static_response._text
+        assert static_response._httpx_response is None
+        assert {file_url} == static_spider.reachable_urls
+
+    async def test_should_not_called_parse_method_when_file_cannot_be_opened(self, tmp_path, mocker):
+        logger_mock = mocker.patch('logging.Logger.exception')
+        hello_file = tmp_path / 'hello.txt'
+        file_url = hello_file.resolve().as_uri()
+        parse_args = []
+
+        async def parse(spider, response):
+            parse_args.extend([spider, response])
+
+        static_spider = StaticSpider(urls=[file_url], parse=parse)
+        await static_spider._handle_url(file_url)
+
+        assert [] == parse_args
+        logger_mock.assert_any_call('unable to open file %s', file_url)
+        assert {file_url} == static_spider.unreachable_urls
+
+    @respx.mock
+    @pytest.mark.parametrize('status_code', [404, 500])
+    async def test_should_not_called_parse_method_if_httpx_response_is_an_error_one(self, mocker, status_code):
+        parse_args = []
+        url = 'http://foo.com'
+
+        def parse(spider, response):
+            parse_args.extend([spider, response])
+
+        respx.get(url) % status_code
+        logger_mock = mocker.patch('logging.Logger.info')
+        static_spider = StaticSpider(urls=[url], parse=parse)
+        await static_spider._handle_url(url)
+
+        assert [] == parse_args
+        logger_mock.assert_any_call('fetching url %s returns an error with status code %s', url, status_code)
+
+    @respx.mock
+    async def test_should_fetch_content_when_giving_http_url(self):
+        parse_args = []
+        url = 'http://foo.com'
+
+        async def parse(spider, response):
+            parse_args.extend([spider, response])
+
+        respx.get(url) % {'text': 'http content'}
+        static_spider = StaticSpider(urls=[url], parse=parse)
+        await static_spider._handle_url(url)
+
+        assert parse_args[0] is static_spider
+        static_response = parse_args[1]
+        assert isinstance(static_response, StaticResponse)
+        assert '' == static_response._url
+        assert '' == static_response._text
+        assert 200 == static_response._httpx_response.status_code
+        assert 'http content' == static_response._httpx_response.text
+        assert 1 == static_spider.request_counter
+        assert static_spider._total_fetch_time > 0
+
+    @respx.mock
+    async def test_should_raise_errors_if_parse_function_raises_error_and_ignore_errors_is_false(self):
+        async def parse(*_):
+            raise ValueError('just a test')
+
+        url = 'http://foo.com'
+        respx.get(url)
+        static_spider = StaticSpider(urls=[url], parse=parse, ignore_errors=False)
+
+        with pytest.raises(ValueError) as exc_info:
+            await static_spider._handle_url(url)
+
+        assert 'just a test' == str(exc_info.value)
+
+    @respx.mock
+    async def test_should_not_raise_error_if_parse_function_raises_error_and_ignore_errors_is_true(self):
+        async def parse(*_):
+            raise ValueError('just a test')
+
+        url = 'http://foo.com'
+        respx.get(url)
+        static_spider = StaticSpider(urls=[url], parse=parse, ignore_errors=True)
+
+        try:
+            await static_spider._handle_url(url)
+        except ValueError:
+            pytest.fail('ValueError was raised and it should not happen')
+
+    # save_item tests
+
+    async def test_should_call_item_processors_and_reject_item_if_one_processor_returns_none(self, capsys, mocker):
+        logger_mock = mocker.patch('logging.Logger.debug')
+        data = {'banana': True}
+
+        def processor_1(item):
+            print("I'm a processor")
+            return item
+
+        async def processor_2(item):
+            await anyio.sleep(0)
+            if 'banana' in item:
+                return
+            return item
+
+        config = Configuration(item_processors=[processor_1, processor_2])
+        static_spider = StaticSpider(urls=['http://foo.com'], parse=lambda x, y: None, config=config)
+        await static_spider.save_item(data)
+
+        logger_mock.assert_any_call('item %s was rejected', data)
+        out, _ = capsys.readouterr()
+        assert "I'm a processor" in out
+
+    async def test_should_save_content_to_backup_file(self, tmp_path, capsys):
+
+        def processor(item):
+            print("I'm a processor")
+            return item
+
+        backup = tmp_path / 'backup.mp'
+        fruit_1 = {'fruit': 'pineapple'}
+        fruit_2 = {'fruit': 'orange'}
+        config = Configuration(backup_filename=f'{backup.resolve()}', item_processors=[processor])
+        static_spider = StaticSpider(urls=['https://foo.com'], parse=lambda x, y: None, config=config)
+        await static_spider.save_item(fruit_1)
+        await static_spider.save_item(fruit_2)
+        out, _ = capsys.readouterr()
+
+        assert [fruit_1, fruit_2] == [item async for item in read_mp(f'{backup.resolve()}')]
+        assert "I'm a processor" in out
+
+    # _get_request_delay tests
+
+    @respx.mock
+    @pytest.mark.parametrize(('robots_content', 'value'), [
+        ('Disallow: /', -1),
+        ('Crawl-delay: 2', 2)
+    ])
+    async def test_should_return_robots_txt_value_when_follow_robots_txt_is_true(self, robots_content, value):
+        url = 'http://foo.com'
+        respx.get(f'{url}/robots.txt') % {'text': f'User-agent:*\n{robots_content}'}
+        static_spider = StaticSpider(urls=[url], parse=lambda x, y: None, config=Configuration(follow_robots_txt=True))
+
+        assert value == await static_spider._get_request_delay(url)
+
+    @respx.mock
+    async def test_should_return_config_delay_when_follow_robots_txt_is_false(self):
+        url = 'http://foo.com'
+        request = respx.get(f'{url}/robots.txt') % {'text': 'User-agent:*\nDisallow: '}
+        config = Configuration(min_request_delay=3, max_request_delay=3)
+        static_spider = StaticSpider(urls=[url], parse=lambda x, y: None, config=config)
+
+        assert not request.called
+        assert 3 == await static_spider._get_request_delay(url)
+
+    # simple test of run and statistics methods, more reliable tests are below
+
+    @respx.mock
+    # it is very weird but when I try to combine this test with the next one (to have only one test) I get
+    # a strange error related to mocking. This is why I have two tests, one to check exclusion works and another
+    # to check the rest of the logic
+    async def test_should_exclude_url_when_robots_txt_excludes_it(self):
+        url = 'http://foo.com'
+        respx.get(f'{url}/robots.txt') % 401
+
+        async def parse(*_) -> None:
+            pass
+
+        static_spider = StaticSpider(urls=[url], parse=parse, config=Configuration(follow_robots_txt=True))
+        await static_spider.run()
+        assert static_spider.reachable_urls == set()
+        assert static_spider.robots_excluded_urls == {url}
+
+    @respx.mock
+    async def test_should_return_correct_statistics_after_running_spider(self):
+        url1 = 'http://foo.com'
+        respx.get(url1, path='/')
+        respx.get(f'{url1}', path='/robots.txt') % 404
+
+        async def parse(*_) -> None:
+            pass
+
+        static_spider = StaticSpider(urls=[url1], parse=parse, config=Configuration(follow_robots_txt=True))
+        await static_spider.run()
+        stats = static_spider.statistics()
+
+        assert stats.reachable_urls == {url1}
+        assert stats.unreachable_urls == set()
+        assert stats.followed_urls == set()
+        assert stats.robot_excluded_urls == set()
+        assert stats.request_counter == 1
+        assert stats.total_time > 0
+        assert stats.average_fetch_time > 0
+
+
+class TestIntegrationStaticSpider:
+    """More concrete tests of StaticSpider class"""
+
+    @staticmethod
+    def processor(item: dict) -> dict:
+        item['date'] = datetime.now()
+        return item
+
+    @staticmethod
+    async def parse(spider: StaticSpider, response: StaticResponse):
+        quotes = [quote.strip() for quote in response.xpath('//blockquote/p/text()').getall()]
+        authors = [author.strip() for author in response.css('blockquote footer::text').getall()]
+        for quote, author in zip(quotes, authors):
+            await spider.save_item({
+                'quote': quote,
+                'author': author
+            })
+
+        link = response.xpath('//a[2][contains(@href, "page")]/@href').get()
+        if link is not None:
+            await response.follow(link)
+
+    @staticmethod
+    async def common_assert(stats: SpiderStatistics, backup_path: Path):
+        assert stats.unreachable_urls == set()
+        assert stats.robot_excluded_urls == set()
+        assert stats.total_time > 0
+
+        albert_count = 0
+        async for item in read_mp(backup_path, decoder=datetime_decoder):
+            assert isinstance(item['date'], datetime)
+            if item['author'] == 'Albert Einstein':
+                albert_count += 1
+
+        assert albert_count == 3
+
+    async def test_should_work_with_file_url(self, page_1_file_url, tmp_path):
+        backup_path = tmp_path / 'backup.mp'
+        config = Configuration(item_processors=[self.processor], backup_filename=f'{backup_path}')
+        static_spider = StaticSpider(urls=[page_1_file_url], parse=self.parse, config=config)
+        await static_spider.run()
+        stats = static_spider.statistics()
+        followed_urls = {
+            page_1_file_url.replace('1', '2').replace('///', '/'),
+            page_1_file_url.replace('1', '3').replace('///', '/')
+        }
+
+        assert stats.reachable_urls == {page_1_file_url} | followed_urls
+        assert stats.followed_urls == followed_urls
+        assert 3 == stats.request_counter
+        assert stats.average_fetch_time == static_spider._total_fetch_time / stats.request_counter
+        await self.common_assert(stats, backup_path)
+
+    @respx.mock
+    async def test_should_work_with_http_url(self, page_content, tmp_path):
+        url = 'http://quotes.com'
+        respx.get(url, path='/robots.txt') % 404
+        respx.get(url, path='/') % {'html': page_content('page1.html')}
+        for i in range(2, 4):
+            respx.get(url, path=f'/page{i}.html') % {'html': page_content(f'page{i}.html')}
+
+        backup_path = tmp_path / 'backup.mp'
+        config = Configuration(
+            item_processors=[self.processor],
+            backup_filename=f'{backup_path}',
+            follow_robots_txt=True
+        )
+        static_spider = StaticSpider(urls=[url], parse=self.parse, config=config)
+        await static_spider.run()
+        stats = static_spider.statistics()
+        followed_urls = {f'{url}/page{i}.html' for i in range(2, 4)}
+
+        assert stats.reachable_urls == {url} | followed_urls
+        assert stats.followed_urls == followed_urls
+        assert stats.request_counter == 3
+        assert stats.average_fetch_time > 0
+        await self.common_assert(stats, backup_path)
